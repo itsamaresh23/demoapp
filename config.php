7c77b9249dbc1f3487934402af37266da29b056a
Amaresh Mohapatra <95702804+itsamaresh23@users.noreply.github.com>
2022-03-16 11:55:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'appdemo.mysql.database.azure.com');
+define('DB_USERNAME', 'azureuser');
+define('DB_PASSWORD', 'Amaresh12345');
 define('DB_NAME', 'appdb');
  
 /* Attempt to connect to MySQL database */
